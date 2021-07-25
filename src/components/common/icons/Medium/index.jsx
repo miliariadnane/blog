6a560edc887dd0060ae888e5554635962ea2e6fc
miliariadnane <miliari.adnane@gmail.com>
2021-07-25 16:48:59 +0100

    dark mode socialBar

--- src/components/common/icons/Medium/index.jsx
+++ src/components/common/icons/Medium/index.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-
+import { ThemeContext } from '../../../../providers/ThemeProvider';
 import RotateLinkImg from '../../RotateLinkImg';
 import light from '../../../../assets/social/medium/dark.png';
+import dark from '../../../../assets/social/medium/light.png';
 
 function Medium({ username, size }) {
+
+  const { theme } = useContext(ThemeContext);
+
   if (!username) return null;
 
   return (
     <RotateLinkImg
       href={`https://medium.com/@${username}`}
-      src={light}
+      src={theme === 'light' ? light : dark }
       size={size}
     />
   );
--- src/components/common/icons/Twitter/index.jsx
+++ src/components/common/icons/Twitter/index.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-
+import { ThemeContext } from '../../../../providers/ThemeProvider';
 import RotateLinkImg from '../../RotateLinkImg';
 import light from '../../../../assets/social/twitter/dark.png';
+import dark from '../../../../assets/social/twitter/light.png';
 
 function Twitter({ username, size }) {
+
+  const { theme } = useContext(ThemeContext);
+
   if (!username) return null;
 
   return (
     <RotateLinkImg
       href={`https://twitter.com/${username}`}
-      src={light}
+      src={theme === 'light' ? light : dark }
       size={size}
     />
   );
